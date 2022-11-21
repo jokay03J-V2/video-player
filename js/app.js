2b60a74aaeb274f3452d25c6c2ca4e701b4b05b3
jokay03J-V2 <jmagro@edenschool.fr>
2022-11-21 11:44:54 +0100

    update logic application

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 import "../css/style.css"
+const video = document.getElementById("video")
+const playBtn = document.getElementById("play-btn")
+const pauseBtn = document.getElementById("pause-btn")
+const progressBar = document.getElementById("progress-bar")
+
+playBtn.addEventListener("click", (ev) => {
+    video.play()
+    playBtn.style.display = "none"
+    pauseBtn.style.display = "block"
+})
+
+pauseBtn.addEventListener("click", (ev) => {
+    video.pause();
+    playBtn.style.display = "block"
+    pauseBtn.style.display = "none"
+})
+
+video.addEventListener("ended", (ev) => {
+    playBtn.style.display = "block"
+    pauseBtn.style.display = "none"
+    isPlayed = true;
+})
+
+video.addEventListener("timeupdate", (ev) => {
+    progressBar.value = ev.currentTarget.currentTime
+    progressBar.max = ev.currentTarget.duration
+})
+
+document.getElementById("b-10s").addEventListener("click", (ev) => {
+    video.currentTime -= 10
+})
+
+document.getElementById("s-10s").addEventListener("click", (ev) => {
+    video.currentTime += 10
+})
+
+document.getElementById("reset").addEventListener("click", (ev) => {
+    video.currentTime = 0
+})
